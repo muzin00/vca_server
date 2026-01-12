254d1d71bde2e59e57e99af388c2f9386cba617a
myusuke <muzin00@gmail.com>
2026-01-12 15:57:47 +0900

    Add VoiceRepositoryProtocol and VoiceRepository implementations.

--- packages/vca_core/vca_core/interfaces/__init__.py
+++ packages/vca_core/vca_core/interfaces/__init__.py
@@ -1,3 +1,4 @@
 from vca_core.interfaces.speaker_repository import SpeakerRepositoryProtocol
+from vca_core.interfaces.voice_repository import VoiceRepositoryProtocol
 
-__all__ = ["SpeakerRepositoryProtocol"]
+__all__ = ["SpeakerRepositoryProtocol", "VoiceRepositoryProtocol"]
--- packages/vca_store/vca_store/repositories/__init__.py
+++ packages/vca_store/vca_store/repositories/__init__.py
@@ -1,3 +1,4 @@
 from vca_store.repositories.speaker_repository import SpeakerRepository
+from vca_store.repositories.voice_repository import VoiceRepository
 
-__all__ = ["SpeakerRepository"]
+__all__ = ["SpeakerRepository", "VoiceRepository"]
